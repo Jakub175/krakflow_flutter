073e390bc14c6cf05db5247d23ded42c8d4ca069
jakub175 <s175365@student.uken.krakow.pl>
2026-05-07 16:27:28 +0200

    lab8 wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'task_repository.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/task.dart';
+import 'package:flutter/material.dart';
+import '../services/task_api_service.dart';
 void main() {
   runApp(MyApp());
+}
+
+
+class TaskApiService {
+  static const String baseUrl = "https://dummyjson.com";
+  static Future<List<Task>> fetchTasks() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/todos"),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List todos = data["todos"];
+      return todos.map((todo) {
+        return Task(
+          title: todo["todo"],
+          deadline: "brak", // brak w API → mockujemy
+          done: todo["completed"],
+          priority: "średni", // brak w API → mockujemy
+        );
+      }).toList();
+    } else {
+      throw Exception("Błąd pobierania danych");
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -375,6 +403,40 @@ class MyButton extends StatelessWidget {
 // child: Text("Tutaj bedzie formulaz dodawania taska"),
 // ),
 
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  late Future<List<Task>> tasksFuture;
+  @override
+  void initState() {
+    super.initState();
+    tasksFuture = TaskApiService.fetchTasks();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Task>>(
+      future: tasksFuture,
+      builder: (context, snapshot) {
+        final tasks = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+// widget TaskCard dla każdego elementu
+          },
+        );
+      },
+    );
+  }
+}
+
+void initState() {
+  super.initState();
+  tasksFuture = TaskApiService.fetchTasks();
+}
 
 
 
